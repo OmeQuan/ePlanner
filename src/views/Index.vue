e634e212da94d2209a8fac4405cb710d6850eb0d
Breg,Coen C.J <c.breg@student.fontys.nl>
2020-10-19 10:54:59 +0200

    edit existing pages

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4">
+    <div class="w-full flex flex-col items-center">
+      <h1>yoink</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'Lato',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    extend: {
+      colors: {
+        creme: '#F3F1EB',
+      },
+    },
   },
   variants: {},
   plugins: [],
 }
-
